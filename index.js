afd35c885762d0b481e96610d8c5cec7c6748cee
Scarlett Baez <s.baez1010@gmail.com>
2020-06-02 23:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var animal = 'cat'
   return animal
 }
 
-function add2() {
+function add2(zero) {
   return zero + two
   const two = 2
 }
@@ -24,3 +24,4 @@ var funkyFunction = function () {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = ("FUNKY!")
+
